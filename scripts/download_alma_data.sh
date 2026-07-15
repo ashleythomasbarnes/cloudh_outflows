ce76c4ce5ced50bd56c185542a1d09ae9cfdf649
Ashley Barnes <30494539+ashleythomasbarnes@users.noreply.github.com>
2026-07-15 20:07:58 +0200

    Create download_alma_data.sh

---
+++ scripts/download_alma_data.sh
@@ -0,0 +1,17 @@
+wget -P ../data/ https://almascience.eso.org/dataPortal/member.uid___A001_X3621_X2808.Clump_H6_sci.spw25_27_29_31_33_35_37_39_41_43.cont.I.pbcor.fits
+
+wget -P ../data/ https://almascience.eso.org/dataPortal/member.uid___A001_X3621_X2808.Clump_H6_sci.spw31.cube.I.pbcor.fits
+
+wget -P ../data/ https://almascience.eso.org/dataPortal/member.uid___A001_X3621_X2808.Clump_H6_sci.spw33.cube.I.pbcor.fits
+
+wget -P ../data/ https://almascience.eso.org/dataPortal/member.uid___A001_X3621_X2808.Clump_H6_sci.spw43.cube.I.pbcor.fits
+
+wget -P ../data/ https://almascience.eso.org/dataPortal/member.uid___A001_X885_X2af._AGAL035.607-00.244__sci.spw25_27_29_31.cont.I.pbcor.fits
+
+wget -P ../data/ https://almascience.eso.org/dataPortal/member.uid___A001_X885_X2af._AGAL035.607-00.244__sci.spw29.cube.I.pbcor.fits
+
+wget -P ../data/ https://almascience.eso.org/dataPortal/sso/member.uid___A001_X3819_X177._035.522-00.274__sci.spw25_27_29_31.cont.I.selfcal.pbcor.fits
+
+wget -P ../data/ https://almascience.eso.org/dataPortal/sso/member.uid___A001_X3819_X177._035.522-00.274__sci.spw25.cube.I.selfcal.pbcor.fits
+
+wget -P ../data/ https://almascience.eso.org/dataPortal/sso/member.uid___A001_X3819_X177._035.522-00.274__sci.spw27.cube.I.selfcal.pbcor.fits
